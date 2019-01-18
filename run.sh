17cd6ad1abb315d9b34007c79a1e15ac8312cb11
Jack Maguire <jack@med.unc.edu>
2019-01-17 20:19:34 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 A=100
 
-for C in 1 5 10 15; do
+for C in 5 10 15; do
 
-    for d in 0.05 0.1 0.15 0.25; do
+    for d in 0.05 0.1 0.15; do
 
 	D=`echo "$A * $C * $d" | bc | awk -F. '{print $1}'`
 
-	for E in 1 5 10 15; do
+	for E in 5 10 15; do
 
-	    for f in 0.05 0.1 0.15 0.25; do
+	    for f in 0.05 0.1 0.15; do
 
 		F=`echo "$D * $E * $f" | bc | awk -F. '{print $1}'`
 
@@ -28,6 +28,14 @@ for C in 1 5 10 15; do
 			    | sed "s/[[F]]/$F/g" \
 			    | sed "s/[[G]]/$G/g" \
 			    | sed "s/[[H]]/$H/g" >> temp.xml
+
+			cp -r template_dir working_dir
+			cd working_dir
+
+			/usr/bin/time -f "%U" timeout 1h mpirun -n 12 multistage_rosetta_scripts.mpiserialization.linuxgccrelease @ flags
+
+			cd ../
+			rm -rf working_dir
 
 		    done
 
