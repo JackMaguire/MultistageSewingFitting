3fbccf426295eef47b5dbd75549071b02b423537
Jack Maguire <jack@med.unc.edu>
2019-01-25 15:29:07 -0500

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -26,72 +26,48 @@
 #         <Sort filter="weighted_motif"/>
 # </Stage>
 
-A=100 #number of trajectories
-
-if [ -f _results ]; then
-    echo "Will not overwrite existing _results file" 1>&2
+if [ -f _results.run2 ]; then
+    echo "Will not overwrite existing _results.run2 file" 1>&2
     exit 1
 fi
 
-#echo $A $C $d $D $E $f $F $G $h $H $x $mean_score >> _results
-echo "NTRAJ C d D E f F G h H num_designs mean_score" > _results
+A=1000 #number of trajectories
+time_limit="10h"
 
-for C in 5 10 15; do
+echo "NTRAJ C d D E f F G h H num_designs mean_score" > _results.run2
 
-    for d in 0.05 0.1 0.15; do # d is fraction to keep
-
-	D=`echo "$A * $C * $d" | bc | awk -F. '{print $1}'`
-	if [[ ${#D} -eq "0" ]]; then
-	    continue;
-	elif [[ $D -eq 0 ]]; then
-	    continue;
-	fi
-
-	for E in 5 10 15; do
-
-	    for f in 0.05 0.1 0.15; do # fraction to keep
-
-		F=`echo "$D * $E * $f" | bc | awk -F. '{print $1}'`
-		if [[ ${#F} -eq "0" ]]; then
-		    continue;
-		elif [[ $F -eq 0 ]]; then
-		    continue;
-		fi
-
-		for G in 1 5 10; do
-
-		    for h in 0.1 0.15 0.25; do # fraction to keep
-
-			H=`echo "$F * $G * $h" | bc | awk -F. '{print $1}'`
-
-			if [[ ${#H} -eq "0" ]]; then
-			    continue;
-			elif [[ $H -eq 0 ]]; then
-			    continue;
-			fi
-
-			# create temp script
+#1
+C=5; d=0.05; E=5; f=0.1; G=1; h=0.15; token=1
+D=`echo "$A * $C * $d" | bc | awk -F. '{print $1}'`
+if [[ ${#D} -eq "0" ]] || [[ $D -eq 0 ]]; then
+    #do nothing
+else
+	F=`echo "$D * $E * $f" | bc | awk -F. '{print $1}'`
+	if [[ ${#F} -eq "0" ]] || [[ $F -eq 0 ]]; then
+		#do nothing
+	else	
+		H=`echo "$F * $G * $h" | bc | awk -F. '{print $1}'`
+		if [[ ${#H} -eq "0" ]] || [[ $H -eq 0 ]]; then
+			#do nothing
+		else
+			#Hot Loop!
 			cat template.xml \
-			    | sed "s/##A##/$A/g" \
-			    | sed "s/##C##/$C/g" \
-			    | sed "s/##D##/$D/g" \
-			    | sed "s/##E##/$E/g" \
-			    | sed "s/##F##/$F/g" \
-			    | sed "s/##G##/$G/g" \
-			    | sed "s/##H##/$H/g" > temp.xml
-
-			#exit 0
-
+				| sed "s/##A##/$A/g" \
+				| sed "s/##C##/$C/g" \
+				| sed "s/##D##/$D/g" \
+				| sed "s/##E##/$E/g" \
+				| sed "s/##F##/$F/g" \
+				| sed "s/##G##/$G/g" \
+				| sed "s/##H##/$H/g" > temp.xml
+				
 			cp -r template_dir working_dir
 			cd working_dir
 
 			#RUN
-			#/usr/bin/time -f "%U"
-			#Let's not use /usr/bin/time and just to time counting internally
 			echo Running $A $C $d $D $E $f $F $G $h $H `date`
-			timeout 1h mpirun -n 12 multistage_rosetta_scripts.mpiserialization.linuxgccrelease @ flags
+			timeout $time_limit mpirun -n 12 multistage_rosetta_scripts.mpiserialization.linuxgccrelease @ flags
 
-			cat score.sc >> ../all_scores.sc
+			cat score.sc >> ../all_scores.run2.sc
 
 			#ANALYZE
 			for x in 1 5 10 25 50 100; do
@@ -99,21 +75,31 @@ for C in 5 10 15; do
 				mean_score=$(awk -v c1=MS_weighted -f ../print_column_no_header.awk score.sc \
 						 | sort -nk1 | head -n $x | awk -f ../average.awk
 					  )
-				echo $A $C $d $D $E $f $F $G $h $H $x $mean_score >> ../_results
+				echo $A $C $d $D $E $f $F $G $h $H $x $mean_score >> ../_results.run2
 			    fi
 			done
 
 			cd ../
 			rm -rf working_dir
 
-		    done
+		fi
+	fi #F
+fi #D
 
-		done
+C=5; d=0.05; E=5; f=0.05; G=5; h=0.15; token=2
+C=10; d=0.05; E=5; f=0.05; G=1; h=0.25; token=3
+C=5; d=0.05; E=10; f=0.15; G=5; h=0.1; token=4
+C=10; d=0.05; E=10; f=0.1; G=10; h=0.15; token=5
+C=5; d=0.1; E=15; f=0.15; G=5; h=0.15; token=6
+C=5; d=0.15; E=10; f=0.05; G=10; h=0.25; token=7
+C=10; d=0.1; E=5; f=0.15; G=1; h=0.25; token=8
+C=5; d=0.1; E=5; f=0.05; G=10; h=0.25; token=9
+C=10; d=0.05; E=5; f=0.05; G=5; h=0.1; token=10
+C=10; d=0.05; E=5; f=0.15; G=1; h=0.15; token=11
+C=5; d=0.1; E=5; f=0.05; G=10; h=0.15; token=12
 
-	    done #F
 
-	done #E
 
-    done #D
+#echo $A $C $d $D $E $f $F $G $h $H $x $mean_score >> _results
 
-done #C
+
