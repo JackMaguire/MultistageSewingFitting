5ce1c07e0e0e9c1c048247b4883fdc7812b89495
Jack Maguire <jack@med.unc.edu>
2019-01-28 13:42:59 -0500

    Create run3.sh

---
+++ run3.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Variables:
+# <Stage num_runs_per_input_struct="##A##" total_num_results_to_keep="1">
+#         <Add mover_name="virt"/>
+#         <Add mover_name="root"/>
+#         Sort filter="weighted_motif"/>
+#         <Sort filter="true_filter"/>
+# </Stage>
+# <Stage num_runs_per_input_struct="##C##" total_num_results_to_keep="##D##">
+#         <Add mover_name="t1"/>
+#         <Add mover_name="chim"/>
+#         <Add mover_name="t2"/>
+#         <Sort filter="weighted_motif"/>
+# </Stage>
+# <Stage num_runs_per_input_struct="##E##" total_num_results_to_keep="##F##">
+#         <Add mover_name="t3"/>
+#         <Add mover_name="shear_move_gsa"/>
+#         <Add mover_name="t4"/>
+#         <Sort filter="weighted_motif"/>
+# </Stage>
+# <Stage num_runs_per_input_struct="##G##" total_num_results_to_keep="##H##">
+#         <Add mover_name="t5"/>
+#         <Add mover_name="small_move_gsa"/>
+#         <Add mover_name="t6"/>
+#         <Sort filter="weighted_motif"/>
+# </Stage>
+
+A=100 #number of trajectories
+
+if [ -f _results ]; then
+    echo "Will not overwrite existing _results file" 1>&2
+    exit 1
+fi
+
+#echo $A $C $d $D $E $f $F $G $h $H $x $mean_score >> _results
+echo "NTRAJ C d D E f F G h H num_designs mean_score" > _results
+
+for C in 5 10 15; do
+
+    for d in 0.05 0.1 0.15; do # d is fraction to keep
+
+	D=`echo "$A * $C * $d" | bc | awk -F. '{print $1}'`
+	if [[ ${#D} -eq "0" ]]; then
+	    continue;
+	elif [[ $D -eq 0 ]]; then
+	    continue;
+	fi
+
+	for E in 5 10 15; do
+
+	    for f in 0.05 0.1 0.15; do # fraction to keep
+
+		F=`echo "$D * $E * $f" | bc | awk -F. '{print $1}'`
+		if [[ ${#F} -eq "0" ]]; then
+		    continue;
+		elif [[ $F -eq 0 ]]; then
+		    continue;
+		fi
+
+		for G in 1 5 10; do
+
+		    for h in 0.1 0.15 0.25; do # fraction to keep
+
+			H=`echo "$F * $G * $h" | bc | awk -F. '{print $1}'`
+
+			if [[ ${#H} -eq "0" ]]; then
+			    continue;
+			elif [[ $H -eq 0 ]]; then
+			    continue;
+			fi
+
+			# create temp script
+			cat template.xml \
+			    | sed "s/##A##/$A/g" \
+			    | sed "s/##C##/$C/g" \
+			    | sed "s/##D##/$D/g" \
+			    | sed "s/##E##/$E/g" \
+			    | sed "s/##F##/$F/g" \
+			    | sed "s/##G##/$G/g" \
+			    | sed "s/##H##/$H/g" > temp.xml
+
+			#exit 0
+
+			cp -r template_dir working_dir
+			cd working_dir
+
+			#RUN
+			#/usr/bin/time -f "%U"
+			#Let's not use /usr/bin/time and just to time counting internally
+			echo Running $A $C $d $D $E $f $F $G $h $H `date`
+			timeout 1h mpirun -n 12 multistage_rosetta_scripts.mpiserialization.linuxgccrelease @ flags
+
+			cat score.sc >> ../all_scores.sc
+
+			#ANALYZE
+			for x in 1 5 10 25 50 100; do
+			    if [[ `grep vav1_start_node_and_partner score.sc | wc -l` -ge $x ]]; then
+				mean_score=$(awk -v c1=MS_weighted -f ../print_column_no_header.awk score.sc \
+						 | sort -nk1 | head -n $x | awk -f ../average.awk
+					  )
+				echo $A $C $d $D $E $f $F $G $h $H $x $mean_score >> ../_results
+			    fi
+			done
+
+			cd ../
+			rm -rf working_dir
+
+		    done
+
+		done
+
+	    done #F
+
+	done #E
+
+    done #D
+
+done #C
